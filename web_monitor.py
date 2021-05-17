8e0651d8e1ae24a70dee3a0330fa19ad3e6874cb
Santhosh Balasa <santhosh.kbr@gmail.com>
2021-05-17 19:35:16 +0530

    Removed unused logger var

--- web_monitor.py
+++ web_monitor.py
@@ -15,9 +15,8 @@ import datetime
 
 from urllib import request, error
 
-logger = logging.getLogger(__name__)
 logging.basicConfig(
-    format="%(asctime)s %(levelname)-8s %(message)s",
+    format=f"%(asctime)s %(levelname)-8s %(message)s",
     level=logging.INFO,
     datefmt="%Y-%m-%d %H:%M:%S",
 )
